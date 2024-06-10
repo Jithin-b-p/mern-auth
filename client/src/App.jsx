828f364ab032cdd8e34edd3ae5aabb0ec4848502
unknown <jithinbp007@gmail.com>
2024-06-10 22:18:18 +0530

    add routes and create pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,24 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Profile from "./pages/Profile";
+import Signin from "./pages/Signin";
+import Signup from "./pages/Signup";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+
+  { path: "/about", element: <About /> },
+  { path: "/profile", element: <Profile /> },
+  { path: "/signin", element: <Signin /> },
+  { path: "/signup", element: <Signup /> },
+]);
+
 function App() {
-  return <h1 className="text-red-600">App</h1>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
