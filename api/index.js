6a7cb311458b61058da24c0f7d94c3bd36f93011
unknown <jithinbp007@gmail.com>
2024-06-11 22:56:55 +0530

    creating a connection to DB

--- api/index.js
+++ api/index.js
@@ -1,5 +1,18 @@
 import express from "express";
+import mongoose from "mongoose";
+import { connectDB } from "./db/connect.db.js";
 
 const app = express();
+const port = process.env.PORT || 3000;
 
-app.listen(3000, () => console.log("Server start running at port 3000..."));
+const start = async () => {
+  try {
+    await connectDB();
+    console.log("connnected to DB..");
+    app.listen(port, () => console.log("Server start running at port 3000..."));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
