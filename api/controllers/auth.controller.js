1d851a19e3cab5bb3f49c07191c4aba56f899966
unknown <jithinbp007@gmail.com>
2024-06-13 22:38:09 +0530

    ceating an asyncwrapper to reduce boilerplate code

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,38 +1,31 @@
+import { asyncWrapper } from "../middleware/asyncWrapper.js";
 import User from "../models/user.model.js";
 import { compareHashPassword, hashPassword } from "../util/hashing.js";
 
-export const signup = async (req, res) => {
+export const signup = asyncWrapper(async (req, res, next) => {
   const { username, email, password } = req.body;
 
   const hashedPassword = hashPassword(password);
-  try {
-    const newUser = await User.create({
-      username,
-      email,
-      password: hashedPassword,
-    });
-    res.status(201).json({ status: "success", data: newUser });
-  } catch (error) {
-    res.status(500).json({ status: "failure", message: error.message });
-  }
-};
+  const newUser = await User.create({
+    username,
+    email,
+    password: hashedPassword,
+  });
+  res.status(201).json({ status: "success", data: newUser });
+});
 
-export const login = async (req, res) => {
+export const login = asyncWrapper(async (req, res) => {
   const { username, password } = req.body;
 
-  try {
-    const user = await User.findOne({ username });
+  const user = await User.findOne({ username });
 
-    const success = compareHashPassword(password, user.password);
+  const success = compareHashPassword(password, user.password);
 
-    if (!success) {
-      return res
-        .status(404)
-        .json({ status: "failure", message: "invalid password!" });
-    }
-
-    res.status(200).json({ status: "success", message: "login successfully!" });
-  } catch (error) {
-    res.status(500).json({ status: "failure", message: error.message });
+  if (!success) {
+    return res
+      .status(404)
+      .json({ status: "failure", message: "invalid password!" });
   }
-};
+
+  res.status(200).json({ status: "success", message: "login successfully!" });
+});
