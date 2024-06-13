28b820f51079f4db3703557cc4463eb9be10c886
unknown <jithinbp007@gmail.com>
2024-06-13 20:03:17 +0530

    create routes and controllers for auth

--- api/index.js
+++ api/index.js
@@ -2,11 +2,15 @@ import express from "express";
 import mongoose from "mongoose";
 import { connectDB } from "./db/connect.db.js";
 import userRouter from "./routes/user.route.js";
+import authRouter from "./routes/auth.route.js";
 
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use(express.json());
+
 app.use("/api/user", userRouter);
+app.use("/api/auth", authRouter);
 
 const start = async () => {
   try {
