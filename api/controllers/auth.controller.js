42073a39c26aef62b17bec8bf052445fb647b39a
unknown <jithinbp007@gmail.com>
2024-06-13 22:14:09 +0530

    create login controller

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,12 +1,38 @@
 import User from "../models/user.model.js";
+import { compareHashPassword, hashPassword } from "../util/hashing.js";
 
 export const signup = async (req, res) => {
   const { username, email, password } = req.body;
 
+  const hashedPassword = hashPassword(password);
   try {
-    const newUser = await User.create(req.body);
-    res.status(200).json({ status: "success", data: newUser });
+    const newUser = await User.create({
+      username,
+      email,
+      password: hashedPassword,
+    });
+    res.status(201).json({ status: "success", data: newUser });
   } catch (error) {
-    res.status(500).json({ status: "failure", data: error.message });
+    res.status(500).json({ status: "failure", message: error.message });
+  }
+};
+
+export const login = async (req, res) => {
+  const { username, password } = req.body;
+
+  try {
+    const user = await User.findOne({ username });
+
+    const success = compareHashPassword(password, user.password);
+
+    if (!success) {
+      return res
+        .status(404)
+        .json({ status: "failure", message: "invalid password!" });
+    }
+
+    res.status(200).json({ status: "success", message: "login successfully!" });
+  } catch (error) {
+    res.status(500).json({ status: "failure", message: error.message });
   }
 };
