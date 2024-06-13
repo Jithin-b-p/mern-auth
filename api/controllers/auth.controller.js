8f66b2217ae1ae204da009b677df8d0b34c5fd8f
unknown <jithinbp007@gmail.com>
2024-06-13 21:11:57 +0530

    create signup controller

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,3 +1,12 @@
-export const signup = (req, res) => {
-  res.send("signup");
+import User from "../models/user.model.js";
+
+export const signup = async (req, res) => {
+  const { username, email, password } = req.body;
+
+  try {
+    const newUser = await User.create(req.body);
+    res.status(200).json({ status: "success", data: newUser });
+  } catch (error) {
+    res.status(500).json({ status: "failure", data: error.message });
+  }
 };
