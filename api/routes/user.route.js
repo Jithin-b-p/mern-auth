558bbe9f2919afd4b35736ba93cf8005f49aa821
unknown <jithinbp007@gmail.com>
2024-06-13 19:15:19 +0530

    create controller

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,9 +1,8 @@
 import { Router } from "express";
+import { getUser } from "../controllers/user.controller.js";
 
 const router = Router();
 
-router.get("/", (req, res) => {
-  res.send("Hello user!");
-});
+router.get("/", getUser);
 
 export default router;
