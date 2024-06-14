d271c2443209bb61b054c8fde23f5aa8d88caf23
unknown <jithinbp007@gmail.com>
2024-06-14 13:52:00 +0530

    creating signup UI

--- client/src/pages/Signin.jsx
+++ client/src/pages/Signin.jsx
@@ -1,5 +1,12 @@
+import { Link } from "react-router-dom";
+
 function Signin() {
-  return <div>Signin</div>;
+  return (
+    <div>
+      Signin
+      <Link to="/signup">signup</Link>
+    </div>
+  );
 }
 
 export default Signin;
--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -1,5 +1,46 @@
+import { Link } from "react-router-dom";
+
 function Signup() {
-  return <div>Signup</div>;
+  return (
+    <form className="flex flex-col justify-center w-4/5 gap-5 mx-auto text-center">
+      <h1 className="mb-5 text-2xl font-bold">Sign up</h1>
+      <input
+        className="px-3 py-2 rounded-md bg-slate-50"
+        id="username"
+        type="text"
+        placeholder="username"
+      />
+
+      <input
+        className="px-3 py-2 rounded-md bg-slate-50"
+        id="email"
+        type="email"
+        placeholder="email"
+      />
+
+      <input
+        className="px-3 py-2 rounded-md bg-slate-50"
+        id="password"
+        type="password"
+        placeholder="password"
+      />
+      <button
+        type="submit"
+        className="px-3 py-2 font-medium text-white uppercase bg-blue-900 rounded-md"
+      >
+        Sign up
+      </button>
+      <button className="px-3 py-2 font-medium text-white uppercase bg-red-600 rounded-md">
+        Continue with Google
+      </button>
+      <span>
+        Have an account?{" "}
+        <Link className="text-blue-600" to="/signin">
+          signin
+        </Link>
+      </span>
+    </form>
+  );
 }
 
 export default Signup;
